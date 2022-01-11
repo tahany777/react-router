9b533eff79981f505f70f654f1aa25332c7a046b
tahany <tahanymohammed777@hotmail.com>
2022-01-11 23:34:37 +0200

    edit data

--- src/App.js
+++ src/App.js
@@ -14,21 +14,21 @@ function App() {
     {
       id: 1,
       title:
-        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        "post one",
       datetime: "July 01, 2022 11:17:36 AM",
-      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+      body: "First post",
     },
     {
       id: 2,
-      title: "qui est esse",
+      title: "post Two",
       datetime: "July 01, 2022 11:17:36 AM",
-      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+      body: "second post",
     },
     {
       id: 3,
-      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      title: "post three",
       datetime: "July 01, 2022 11:17:36 AM",
-      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+      body: "third post",
     },
   ]);
   const [search, setSearch] = useState("");
