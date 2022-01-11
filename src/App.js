b5c3f9fed9b907321f8ea4175a867e31d028d0bb
tahany <tahanymohammed777@hotmail.com>
2022-01-11 23:17:12 +0200

    feat: add new post delete post and search for a post

--- src/App.js
+++ src/App.js
@@ -6,18 +6,83 @@ import NewPost from "./NewPost";
 import PostPage from "./PostPage";
 import About from "./About";
 import Missing from "./Missing";
-import { Route, Routes, useHistory } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-
+import { format } from "date-fns";
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      datetime: "July 01, 2022 11:17:36 AM",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    },
+    {
+      id: 2,
+      title: "qui est esse",
+      datetime: "July 01, 2022 11:17:36 AM",
+      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+    },
+    {
+      id: 3,
+      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      datetime: "July 01, 2022 11:17:36 AM",
+      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+    },
+  ]);
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const navigate = useNavigate();
+  useEffect(() => {
+    const filteredResult = posts.filter(
+      (post) =>
+        post.body.toLowerCase().includes(search.toLowerCase()) ||
+        post.title.toLowerCase().includes(search.toLowerCase())
+    );
+    setSearchResult(filteredResult.reverse());
+  }, [posts, search]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const dateTime = format(new Date(), "MMMM dd, yyyy pp");
+    const newPost = { id, title: postTitle, dateTime, body: postBody };
+    const allPosts = [ ...posts, newPost ];
+    setPosts(allPosts);
+    setPostTitle("");
+    setPostBody("");
+    navigate("/");
+  };
+  const handleDelete = (id) => {
+    const postsList = posts.filter((post) => post.id !== id);
+    setPosts(postsList);
+    navigate("/");
+  };
   return (
     <div className="App">
-      <Header />
-      <Nav />
+      <Header title="React JS Blog" />
+      <Nav search={search} setSearch={setSearch} />
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/post" element={<NewPost />} />
-        <Route path="/post/:id" element={<PostPage />} />
+        <Route exact path="/" element={<Home posts={searchResult} />} />
+        <Route
+          exact
+          path="/post"
+          element={
+            <NewPost
+              handleSubmit={handleSubmit}
+              postTitle={postTitle}
+              setPostTitle={setPostTitle}
+              postBody={postBody}
+              setPostBody={setPostBody}
+            />
+          }
+        />
+        <Route
+          path="/post/:id"
+          element={<PostPage posts={posts} handleDelete={handleDelete} />}
+        />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Missing />} />
       </Routes>
